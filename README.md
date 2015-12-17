2bce3b45e12950f2347e957eb91d4aa564e68679
vgdn1942 <vgdn1942@gmail.com>
2015-12-17 12:28:16 +0300

    Update README

---
+++ README.md
@@ -0,0 +1,3 @@
+# android_device_lenovo_P70
+
+cm-12.1 device tree for Lenovo P70-A
---
+++ extract-files.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VENDOR=lenovo
+DEVICE=P70
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/##g'`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    adb pull /system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
